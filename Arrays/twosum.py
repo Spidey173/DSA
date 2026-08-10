2cf49159e70b923656b3b0831df0e22669e677bb
Spidey . <spidey.@Spideys-MacBook-Air.local>
2026-08-10 14:03:04 +0530

    Add Way 2 (all outcomes) and Way 3 (first outcome only)

--- Arrays/twosum.py
+++ Arrays/twosum.py
@@ -1,6 +1,6 @@
 # Question: Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
 
-# Way 1: Direct iteration (Brute Force)
+# Way 1: Direct iteration (Brute Force - print directly)
 a = [2, 7, 11, 15]
 target = 18
 n = len(a)
@@ -9,16 +9,24 @@ for i in range(n):
         if a[i] + a[j] == target:
             print([i, j])
 
-# Way 2: Function-based approach (Brute Force)
-def twoSum(nums, target):
+# Way 2: Function returning all possible outcomes
+def twoSumAll(nums, target):
     ans = []
     n = len(nums)
-
     for i in range(n):
         for j in range(i + 1, n):
             if nums[i] + nums[j] == target:
                 ans.append([i, j])
-
     return ans
 
-print(twoSum([1,2,3,4,5], 5))
+print(twoSumAll([1, 2, 3, 4, 5], 5))
+
+# Way 3: Function returning only the 1st outcome
+def twoSumFirst(nums, target):
+    n = len(nums)
+    for i in range(n):
+        for j in range(i + 1, n):
+            if nums[i] + nums[j] == target:
+                return [i, j]
+
+print(twoSumFirst([1, 2, 3, 4, 5], 5))

e2301a5b4a0d3861107b4048e4927541de96cfd6
Pruthvi R <pruthvir6666@gmail.com>
2026-08-16 01:57:53 +0530

    Add clean solutions for Best Time to Buy and Sell Stock

---
+++ Arrays/besttime.py
@@ -0,0 +1,35 @@
+# Question: LeetCode 121 - Best Time to Buy and Sell Stock
+# Find the maximum profit you can achieve by buying on one day and selling in the future.
+
+# Way 1: One-Pass with Explicit If Statements (Time: O(N), Space: O(1))
+class Solution1:
+    def maxProfit(self, prices):
+        min_price = float('inf')
+        max_profit = 0
+
+        for price in prices:
+            if price < min_price:
+                min_price = price
+            profit = price - min_price
+            if profit > max_profit:
+                max_profit = profit
+
+        return max_profit
+
+sol1 = Solution1()
+print(sol1.maxProfit([7, 1, 5, 3, 6, 4]))
+
+# Way 2: One-Pass using min() and max() (Time: O(N), Space: O(1))
+class Solution2:
+    def maxProfit(self, prices):
+        min_price = float('inf')
+        max_profit = 0
+
+        for price in prices:
+            min_price = min(min_price, price)
+            max_profit = max(max_profit, price - min_price)
+
+        return max_profit
+
+sol2 = Solution2()
+print(sol2.maxProfit([7, 1, 5, 3, 6, 4]))

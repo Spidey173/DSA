620860dd08ba0bd0e6875d077dd4dbf691fb7b6b
Pruthvi R <pruthvir6666@gmail.com>
2026-08-23 23:21:28 +0530

    Simplify sum_n_numbers.py: keep only question header

--- Basics/sum_n_numbers.py
+++ Basics/sum_n_numbers.py
@@ -1,5 +1,4 @@
-# Concept: Sum of N Natural Numbers
-# Calculates the sum of numbers from 1 to N (e.g., if N = 5, sum is 1 + 2 + 3 + 4 + 5 = 15).
+# Sum of N Natural Numbers
 
 n = int(input("Enter N: "))
 
@@ -9,7 +8,6 @@ for i in range(1, n + 1):
     sum_for += i
 print("Way 1 (For Loop) Sum =", sum_for)
 
-
 # Way 2: Using a while loop
 sum_while = 0
 i = 1
@@ -18,7 +16,6 @@ while i <= n:
     i += 1
 print("Way 2 (While Loop) Sum =", sum_while)
 
-
-# Way 3: Using the math formula (O(1) time - fastest)
+# Way 3: Using the formula
 sum_formula = n * (n + 1) // 2
 print("Way 3 (Formula) Sum =", sum_formula)

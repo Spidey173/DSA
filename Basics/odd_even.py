a2322024e1fad0f56911ec08ea1efa817786169d
Pruthvi R <pruthvir6666@gmail.com>
2026-08-23 23:21:14 +0530

    Simplify odd_even.py: keep only question header

--- Basics/odd_even.py
+++ Basics/odd_even.py
@@ -1,10 +1,7 @@
-# Concept: Odd or Even
-# A number is even if it is divisible by 2 (remainder is 0 when divided by 2).
-# We check this using the modulo operator (%) which gives the remainder.
+# Odd or Even
 
 num = int(input("Enter a number: "))
 
-# If remainder is 0, it is even. Otherwise, it is odd.
 if num % 2 == 0:
     print(num, "is Even")
 else:

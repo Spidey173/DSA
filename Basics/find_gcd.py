e07d6b5e86adeddbf242c91aaee85323b0a6c90b
Pruthvi R <pruthvir6666@gmail.com>
2026-08-26 02:31:12 +0530

    Add recursive GCD to find_gcd.py with separate inputs

--- Basics/find_gcd.py
+++ Basics/find_gcd.py
@@ -8,3 +8,14 @@ while temp_b > 0:
     temp_a, temp_b = temp_b, temp_a % temp_b
 
 print("GCD of", a, "and", b, "is", temp_a)
+
+
+# Using Recursion
+def gcd_recursive(x, y):
+    if y == 0:
+        return x
+    return gcd_recursive(y, x % y)
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+print("GCD of", a, "and", b, "is", gcd_recursive(a, b))

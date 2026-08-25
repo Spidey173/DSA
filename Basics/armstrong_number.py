e22bf0a24ea4b4bbf5245cb84afd77a8d8adfa2d
Pruthvi R <pruthvir6666@gmail.com>
2026-08-25 23:53:49 +0530

    Add Way 2 recursive solution to armstrong_number.py

--- Basics/armstrong_number.py
+++ Basics/armstrong_number.py
@@ -1,21 +1,32 @@
 # Armstrong Number Check
 
-num = int(input("Enter a number: "))
-
-# Find the number of digits
-power = len(str(num))
-
-# Calculate the sum of digits raised to the power
-temp = num
+# Way 1: Iterative Check (Time: O(log n), Space: O(1))
+num1 = int(input("Enter a number: "))
+power1 = len(str(num1))
+temp = num1
 sum_val = 0
 
 while temp > 0:
     digit = temp % 10
-    sum_val += digit ** power
+    sum_val += digit ** power1
     temp //= 10
 
-# Check Armstrong number
-if sum_val == num:
-    print(num, "is an Armstrong number")
+if sum_val == num1:
+    print("Way 1:", num1, "is an Armstrong number")
 else:
-    print(num, "is not an Armstrong number")
+    print("Way 1:", num1, "is not an Armstrong number")
+
+
+# Way 2: Recursive Check (Time: O(log n), Space: O(log n) call stack)
+def armstrong_sum(n, power):
+    if n == 0:
+        return 0
+    return ((n % 10) ** power) + armstrong_sum(n // 10, power)
+
+num2 = int(input("Enter a number: "))
+power2 = len(str(num2))
+
+if armstrong_sum(num2, power2) == num2:
+    print("Way 2:", num2, "is an Armstrong number")
+else:
+    print("Way 2:", num2, "is not an Armstrong number")

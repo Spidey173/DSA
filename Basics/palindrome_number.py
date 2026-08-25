60729e3df1825750668a7b90d0823a7c380dd8e8
Pruthvi R <pruthvir6666@gmail.com>
2026-08-25 17:57:01 +0530

    Add Way 2 recursive solution to palindrome_number.py

--- Basics/palindrome_number.py
+++ Basics/palindrome_number.py
@@ -1,19 +1,37 @@
 # Palindrome Number Check
 
-num = int(input("Enter a number: "))
+# Way 1: Iterative Check (Time: O(log n), Space: O(1))
+num1 = int(input("Enter a number: "))
 
-if num < 0:
-    print(num, "is NOT a Palindrome")
+if num1 < 0:
+    print("Way 1:", num1, "is NOT a Palindrome")
 else:
-    temp = num
+    temp = num1
     reversed_num = 0
-    
     while temp > 0:
         last_digit = temp % 10
         reversed_num = (reversed_num * 10) + last_digit
         temp //= 10
         
-    if num == reversed_num:
-        print(num, "is a Palindrome")
+    if num1 == reversed_num:
+        print("Way 1:", num1, "is a Palindrome")
     else:
-        print(num, "is NOT a Palindrome")
+        print("Way 1:", num1, "is NOT a Palindrome")
+
+
+# Way 2: Recursive Check (Time: O(log n), Space: O(log n) call stack)
+def reverse_recursive(n, rev=0):
+    if n == 0:
+        return rev
+    return reverse_recursive(n // 10, rev * 10 + (n % 10))
+
+def is_palindrome_recursive(num):
+    if num < 0:
+        return False
+    return num == reverse_recursive(num)
+
+num2 = int(input("Enter a number: "))
+if is_palindrome_recursive(num2):
+    print("Way 2:", num2, "is a Palindrome")
+else:
+    print("Way 2:", num2, "is NOT a Palindrome")

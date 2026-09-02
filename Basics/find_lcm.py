8fdd3ffd95c8d8a04e9ee7834584690e1517717e
Pruthvi R <pruthvir6666@gmail.com>
2026-09-02 14:09:12 +0530

    Update find_lcm.py with iterative and recursive GCD-based solutions

--- Basics/find_lcm.py
+++ Basics/find_lcm.py
@@ -1,13 +1,24 @@
 # Find LCM of Two Numbers
 
+# Iterative (Using GCD Formula)
 a = int(input("Enter first number: "))
 b = int(input("Enter second number: "))
 
 temp_a, temp_b = a, b
-while temp_b > 0:
+while temp_b:
     temp_a, temp_b = temp_b, temp_a % temp_b
 
-gcd = temp_a
-lcm = (a * b) // gcd
+lcm = (a * b) // temp_a
+print("LCM of", a, "and", b, "is", lcm)
 
+
+# Using Recursion
+def gcd(x, y):
+    if y == 0:
+        return x
+    return gcd(y, x % y)
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+lcm = (a * b) // gcd(a, b)
 print("LCM of", a, "and", b, "is", lcm)

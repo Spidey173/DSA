19bd44bd8349eb0409dadf9a4a6c6bc4657c61a9
Pruthvi R <pruthvir6666@gmail.com>
2026-09-02 14:12:47 +0530

    Update largest_of_three.py with if-elif-else and max()

--- Basics/largest_of_three.py
+++ Basics/largest_of_three.py
@@ -1,14 +1,23 @@
 # Largest of Three Numbers
 
+# Using if-elif-else
 a = int(input("Enter first number: "))
 b = int(input("Enter second number: "))
 c = int(input("Enter third number: "))
 
 if a >= b and a >= c:
     largest = a
-elif b >= a and b >= c:
+elif b >= c:
     largest = b
 else:
     largest = c
 
-print("The largest number is", largest)
+print("Largest:", largest)
+
+
+# Using max()
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+c = int(input("Enter third number: "))
+
+print("Largest:", max(a, b, c))
